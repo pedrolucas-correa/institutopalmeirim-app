b6207a7036fed06754db1d436e5dd43f9bd18d1d
pedrolucas-correa <pedrolc.correa@outlook.com>
2024-07-08 23:35:14 -0300

    structure commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './screens/login/login.component';
+import { AuthGuard } from './guards/auth.guard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+
+//   {
+//     path: 'home',
+//     canActivate: [AuthGuard],
+//     // component: FullLayoutComponent,
+//     children: [
+//       {
+//         path: '',
+//         // component: HomeComponent,
+//       },
+//     ],
+//   },
+//   { path: '', redirectTo: '/home', pathMatch: 'full' },
+//   { path: `**`, redirectTo: '/home', pathMatch: 'full' },
+];
